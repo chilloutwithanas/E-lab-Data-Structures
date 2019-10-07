89aaab15ae64319d3b0e7d3d6a8cd3329962853c
Pulkit Sambhavi Singh <49859368+Purukitto@users.noreply.github.com>
2019-10-07 13:41:06 +0530

    Update and rename ST4.cpp to ST4.c

--- ST4.c
+++ ST4.c
@@ -1,27 +1,17 @@
 #include<stdio.h>
-
-// Fills array S[] with span values
 void calculateSpan(int price[], int n, int S[])
 {
-// Span value of first day is always 1
 S[0] = 1;
-
-// Calculate span value of remaining days by linearly checking
-// previous days
 int i; 
 for ( i = 1; i < n; i++)
 {
-S[i] = 1; // Initialize span value
-
-// Traverse left while the next element on left is smaller
-// than price[i]
+S[i] = 1;
 int j; 
 for ( j = i-1; (j>=0)&&(price[i]>=price[j]); j--)
 S[i]++;
 }
 }
 
-// A utility function to print elements of array
 void printArray(int arr[],int n)
 {
 int i;
@@ -29,7 +19,6 @@ for ( i = 0; i < n; i++)
 printf("%d ", arr[i]);
 }
 
-// Driver program to test above function
 int main()
 {
 int size,i,q,price[100];
@@ -40,12 +29,7 @@ scanf("%d",&price[i]);
 }
 int n = (4*size)/sizeof(price[0]);
 int S[n];
-
-// Fill the span values in array S[]
 calculateSpan(price, n, S);
-
-// print the calculated span values
 printArray(S, n);
-
 return 0;
 }
